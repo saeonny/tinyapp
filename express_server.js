35c3c943842f53cc2c66a862114b512083a9b69f
Sae <winny9641@gmail.com>
2022-06-14 00:35:21 -0400

    random numstrings generator function implements

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,11 +17,32 @@ app.get("/urls", (req, res) => {
   res.render("urls_index",templateVars);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //this is the format "/urls/:shortURL" and req.params.shortURL return shortURL which indicated by :
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]} ;
   res.render("urls_show", templateVars);
 });
+
+
+function generateRandomString() {
+  let random = Math.random().toString(20).substr(2, 6);
+  const keys = Object.keys(urlDatabase);
+  while(keys.indexOf(random) !== -1){  // while random string does not exists in the urlDatabase
+    random = Math.random().toString(20).substr(2, 6);
+  }
+  return random;
+
+}
+
 
 
 
