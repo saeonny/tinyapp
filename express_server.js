7ad8fc92c464c29facbb9090d97af2792c6481d7
Sae <winny9641@gmail.com>
2022-06-13 21:55:52 -0400

    app.get practice with res.json

--- express_server.js
+++ express_server.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-//run http://localhost:8080/ this is the home route
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//run http://localhost:8080/ this is the home route
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello!"); //send simple string
+});
+
+
+//http://localhost:8080/urls.json 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase); // send json
 });
 
 app.listen(PORT, () => {
