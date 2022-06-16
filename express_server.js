eb92142bd9264b1cb612675487f71f3de310b530
Sae <winny9641@gmail.com>
2022-06-16 17:51:36 -0400

    make user_id cookie as private

--- express_server.js
+++ express_server.js
@@ -3,12 +3,18 @@ const PORT = 8080; // default port 8080
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session')
 const { redirect, header } = require("express/lib/response");
 const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+//app.use(cookieParser());
+app.use(cookieSession({
+  name : 'session',
+  keys: ["key1","key2"],
+  
+}))
 
 app.set("view engine", "ejs");
 
@@ -19,40 +25,43 @@ let registerTry_invID = false;
 let registerTry_exist = false;
 ///////////////////////////////DATA/////////////////////////////////////////////////////
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
+  // "userRandomID": {
+  //   id: "userRandomID",
+  //   email: "user@example.com",
     
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
+  // },
+  // "user2RandomID": {
+  //   id: "user2RandomID",
+  //   email: "user2@example.com",
     
-  }
+  // }
 }
 
-users.userRandomID.password = bcrypt.hashSync("purple-monkey-dinosaur",10);
-users.user2RandomID.password = bcrypt.hashSync("dishwasher-funk",10);
+
+// users.userRandomID.password = bcrypt.hashSync("purple-monkey-dinosaur",10);
+// users.user2RandomID.password = bcrypt.hashSync("dishwasher-funk",10);
 
 
 const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "userRandomID"
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    userID: "userRandomID"
-  }
+  // "b2xVn2": {
+  //   longURL: "http://www.lighthouselabs.ca",
+  //   userID: "userRandomID"
+  // },
+  // "9sm5xK": {
+  //   longURL: "http://www.google.com",
+  //   userID: "userRandomID"
+  // }
 };
 
 ///////////////////////////////INITIALIZING /HOME PAGE//////////////////
 app.get("/urls", (req, res) => {
   const templateVars = { urls: null, user_id: null, email: null };
-  if (req.cookies.user_id) {
-    templateVars.urls = findRegisteredURL(req.cookies.user_id);
-    templateVars.user_id = req.cookies.user_id
-    templateVars.email = users[req.cookies.user_id].email
+  if (req.session.user_id) {
+    templateVars.urls = findRegisteredURL(req.session.user_id);
+    templateVars.user_id = req.session.user_id
+   // templateVars.email = users[req.session.user_id].email
+   console.log(req.session.user_id)
+   templateVars.email = users[req.session.user_id].email
   }
 
   res.render("urls_index", templateVars);
@@ -62,7 +71,7 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   let recievedLongUrl = req.body.longURL;
   let shortUrl = generateRandomString();
-  urlDatabase[shortUrl] = { longURL: recievedLongUrl, userID: req.cookies.user_id }
+  urlDatabase[shortUrl] = { longURL: recievedLongUrl, userID: req.session.user_id }
   res.redirect("/urls")
 
 });
@@ -102,7 +111,7 @@ app.post("/register", (req, res) => {
     }
     users[id] = { id: id, email: req.body.email, password: bcrypt.hashSync(req.body.password,10) };
     console.log(users);
-    res.cookie("user_id", id);
+    req.session.user_id = id;
     res.redirect("/urls")
   }
 
@@ -137,9 +146,9 @@ app.get("/login", (req, res) => {
   oginTry_noMatch = false
   const templateVars = { user_id: null, email: null, loginTry_noEmail: loginTry_noEmail, loginTry_noMatch: loginTry_noMatch, createTry: createTry };
 
-  if (req.cookies.user_id !== undefined) {
-    templateVars.user_id = req.cookies.user_id
-    templateVars.email = users[req.cookies.user_id].email
+  if (req.session.user_id !== undefined) {
+    templateVars.user_id = req.session.user_id
+    templateVars.email = users[req.session.user_id].email
   }
 
 
@@ -165,7 +174,10 @@ app.post("/logintry", (req, res) => {
   loginTry_noMatch = false;
 
   if (loginCheck(givenId, givenPass)) {
-    res.cookie("user_id", loginCheck(givenId, givenPass))
+    // res.cookie("user_id", loginCheck(givenId, givenPass))
+    req.session.user_id = loginCheck(givenId, givenPass);
+    
+
     return res.redirect("/urls");
   }
   for (let id in users) {
@@ -191,7 +203,8 @@ app.post("/logintry", (req, res) => {
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  //res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/urls")
 
 })
@@ -218,12 +231,12 @@ app.post("/urls/:shortURL/editted", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { user_id: null, email: null };
-  if (req.cookies.user_id) {
-    templateVars.user_id = req.cookies.user_id
-    templateVars.email = users[req.cookies.user_id].email
+  if (req.session.user_id) {
+    templateVars.user_id = req.session.user_id
+    templateVars.email = users[req.session.user_id].email
     res.render("urls_new", templateVars);
   }
-  if (!req.cookies.user_id) {
+  if (!req.session.user_id) {
     createTry = true;
     res.redirect("/login");
   }
@@ -234,12 +247,12 @@ app.get("/urls/new", (req, res) => {
 
 //this is the format "/urls/:shortURL" and req.params.shortURL return shortURL which indicated by :
 app.get("/urls/:shortURL", (req, res) => {
-  if (req.cookies.user_id) {
-    if (getShortURL(req.cookies.user_id).indexOf(req.params.shortURL) !== -1) {
+  if (req.session.user_id) {
+    if (getShortURL(req.session.user_id).indexOf(req.params.shortURL) !== -1) {
       const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user_id: null };
-      templateVars.user_id = req.cookies.user_id
+      templateVars.user_id = req.session.user_id
       templateVars.longURL = urlDatabase[req.params.shortURL].longURL;
-      templateVars.email = users[req.cookies.user_id].email;
+      templateVars.email = users[req.session.user_id].email;
       return res.render("urls_show", templateVars);
     }
     return res.send("You cant access this way");
